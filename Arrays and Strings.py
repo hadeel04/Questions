5a1cf4fd0bde2d8d57c2137d82e49d74db2ea60e
hadeel04 <hadeel-99@live.com>
2023-11-14 12:06:33 +0200

    finish

--- Arrays and Strings.py
+++ Arrays and Strings.py
@@ -3,3 +3,19 @@
 def reverse(string):
     string = string[::-1]
     return string
+
+#-------------------------------------------
+# Function to find max and min in array
+
+def max_min(list):
+    Max = max(list)
+    Min = min(list)
+    return Max,Min
+
+#-------------------------------------------
+# Function to remove duplicates from a sorted array
+
+def remove_duplicates(lst):
+    res = list(dict.fromkeys(lst))
+    return res
+
