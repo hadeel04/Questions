9a1ce72ac9096d0199e877c5048120391e32f297
hadeel04 <hadeel-99@live.com>
2023-11-29 13:18:04 +0200

    still working in it

---
+++ Big O Notation and Time Complexity Analysis.py
@@ -0,0 +1,13 @@
+# The time complexity of the sorting algorithms:
+
+# 1. buble sort Time Complexity:  O(n^2) => n + n-1 + n-2 + .... = n^2
+# 2. merge sort Time Complexity: O(n*log(n))
+# 3. quick sort Time Complexity:  O(n^2)
+
+# the space complexity of the recursive programs:
+
+# 1. the space complexity of the function  to calculate the factorial of a number using recursion : O(n)
+# because there is  n recursive calls .
+# 2. the space complexity of the function  to generate all permutations of a string using recursion: O(n * n!)
+# because The recursion and list to store permutations contribute to this complexity
+# there is  n recursive calls and n! permutations generated
